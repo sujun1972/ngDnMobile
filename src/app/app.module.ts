2a2165a0eaa37f53bf1a70657b82a41f31968d88
sujun1972 <sujun1972@gmail.com>
2017-12-01 10:19:06 +0100

    加入routes和sass以后看起来正常

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { ErrorComponent } from './error/error.component';
+export const routes: Routes = [
+    // { path: '', redirectTo: 'pages', pathMatch: 'full' },
+    // { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+    // { path: 'register', loadChildren: 'app/pages/register/register.module#RegisterModule' },
+    // { path: 'helper', component: HelperComponent, data: { breadcrumb: '帮助中心' }  },
+    { path: '**', component: ErrorComponent }
+  ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    // useHash: true
+});
